945c31340619c481f57d8fb3e9f897e135eeab2b
Aeius <psb6604@gmail.com>
2022-05-23 20:37:20 +0900

    토큰 저장 경로 변경하여 모든 페이지에서 토큰 접근 가능하도록 수정

--- app.py
+++ app.py
@@ -25,6 +25,21 @@ db = client.od_project
 
 @app.route('/')
 def home():
+    token_receive = request.cookies.get('mytoken')
+    print(token_receive)
+    try:
+        # 웹 접속자가 가지고 있는 token을 시크릿키로 디코딩
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보 조회
+        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+        # return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
     return render_template('/index.html')
 
 if __name__ == '__main__':
