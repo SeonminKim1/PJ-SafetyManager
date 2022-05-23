c798eff9167e704513f89cd0be4f45cd5e6ea755
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-05-23 23:28:12 +0900

    Add nav connect proflie page

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
         # payload 안에 id가 들어있습니다. 이 id로 유저정보 조회
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+        user_info = db.USER.find_one({'id': payload['id']})
         # return jsonify({'result': 'success', 'nickname': userinfo['nick']})
 
     except jwt.ExpiredSignatureError:
@@ -42,7 +42,7 @@ def home():
         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-    return render_template('/index.html')
+    return render_template('/index.html', user_info=user_info)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
